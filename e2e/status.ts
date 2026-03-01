6a409c8ea47ad396420fa64a5171a13fdacd9543
moloch-- <875022+moloch--@users.noreply.github.com>
2026-03-01 12:14:03 -0800

    Update e2e to use own server child proc

--- e2e/status.ts
+++ e2e/status.ts
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
 
-type SliverScriptModule = typeof import("../..");
+type SliverScriptModule = typeof import("..");
 
 function findRepoRoot(startDir: string): string | undefined {
   let dir = path.resolve(startDir);
